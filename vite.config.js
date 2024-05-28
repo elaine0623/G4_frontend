96cba1f88b53515a530d55bd205bdc4eca338b6f
elaine0623 <0148004@gmail.com>
2024-05-28 19:50:10 +0800

    在vite.config.js匯入sass

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,13 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VueDevTools()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/scss/style.scss";`
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
