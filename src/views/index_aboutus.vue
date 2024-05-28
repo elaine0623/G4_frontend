bd519bc2b16a9f8fa96e1bf8d6f5407112139f32
Bruce1219 <891219chiang@gmail.com>
2024-05-28 10:15:14 +0800

    font

--- src/views/index_aboutus.vue
+++ src/views/index_aboutus.vue
@@ -1,5 +1,12 @@
 <template>
-  <section>aboutus</section>
+  <section>
+    <h1>關於果籽</h1>
+  </section>
 </template>
 <script></script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  h1{
+    font-family: 'noto serif tc';
+    color: red;
+}
+</style>
