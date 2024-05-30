12d245eebc9da02e2df143369fbf74e5ed24c901
elaine0623 <0148004@gmail.com>
2024-05-30 20:09:26 +0800

    修改json

--- src/main.js
+++ src/main.js
@@ -6,6 +6,17 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
 // import '@/assets/scss/style.scss'
 
 //Vue.createApp(App).mount('#app')
@@ -15,4 +26,4 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+app.component('font-awesome-icon', FontAwesomeIcon).mount('#app')
