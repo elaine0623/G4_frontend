defe78f7ba139961396fab19d0afc2e502f346c8
Aiden911 <forest1991911@mail.com>
2024-06-01 09:32:33 +0800

    新增footer

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <header1 />
   <RouterView />
+  <footer1 />
 </template>
 
 <script>
 import { RouterLink, RouterView } from 'vue-router'
 import './assets/scss/base/_reset.scss'
 import header1 from '@/components/header.vue'
-// import Aboutus from './views/aboutus.vue'
+import footer1 from '@/components/footer.vue'
 
 export default {
   name: 'App',
   components: {
     header1,
     RouterLink,
-    RouterView
+    RouterView,
+    footer1
   }
 }
 </script>
