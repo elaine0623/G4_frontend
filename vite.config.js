f096f244d28869a946264b766cb6f406e8365c83
Ingrid kao <ingrid7613@gmail.com>
2024-06-18 11:27:25 +0800

    fix: 活動路徑名稱以及package.json中的錯誤

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
-// import '@/assets/scss/style.scss'
 
 // https://vitejs.dev/config/
-
 export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
   if (mode === 'prod') {
     // 放準備部署到server的設定
@@ -15,6 +12,7 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
       build: {
         outDir: 'front'
       },
+      plugins: [vue(), VueDevTools()],
       css: {
         preprocessorOptions: {
           scss: {
@@ -22,10 +20,6 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
           }
         }
       },
-      plugins: [
-        vue(),
-        VueDevTools(),
-      ],
       resolve: {
         alias: {
           '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -35,7 +29,6 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
   } else {
     // 放原本的設定
     return {
-      lintOnSave: false,
       plugins: [vue(), VueDevTools()],
       css: {
         preprocessorOptions: {
