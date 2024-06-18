eb5861e2441f74b09d36735bb9d7cffbd682991e
elaine0623 <0148004@gmail.com>
2024-06-18 10:14:42 +0800

    設定檔更新

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,49 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 // import '@/assets/scss/style.scss'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  lintOnSave:false,
-  plugins: [vue(), VueDevTools()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/assets/scss/style.scss";`
+
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  if (mode === 'prod') {
+    // 放準備部署到server的設定
+    return {
+      base: '/cid101/g4/front/',
+      build: {
+        outDir: 'front'
+      },
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: `@import "@/assets/scss/style.scss";`
+          }
+        }
+      },
+      plugins: [
+        vue(),
+        VueDevTools(),
+      ],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
       }
     }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  } else {
+    // 放原本的設定
+    return {
+      lintOnSave: false,
+      plugins: [vue(), VueDevTools()],
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: `@import "@/assets/scss/style.scss";`
+          }
+        }
+      },
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      }
     }
   }
 })
