47d6ac0be01ad5fed635710b4496cdb67214feca
melissa.chouuu <melissa870228@gmail.com>
2024-06-18 21:07:31 +0800

    edit: map added location & scroll bar

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,37 +2,44 @@
 <template>
   <section>
   <h2>關於果籽</h2>
+  <!-- <img class="title-bgi" src="@/assets/image/deco.png" alt=""> -->
   <div class="aboutus">
     <div class="card">
-      <p>食安問題層出不窮，架設果籽網站，期望能讓民眾重視食品安全，宣導購買原型食物及有機耕種的觀念，支持在地小農以友善土地的方式耕種，讓民眾吃得健康，土地也得以永續</p>
-      <img src="" alt="">
+          <p>食安問題層出不窮，架設果籽網站，期望能讓民眾重視食品安全，宣導購買原型食物及有機耕種的觀念，支持在地小農以友善土地的方式耕種，讓民眾吃得健康，土地也得以永續</p>
+          <div class="pic-left-intro-up">
+            <img src="../assets/image/index_about2.png" alt="garden1">
+            <div class="img-circle" v-for="i in 3" :key="i" :class="'circle' + i">
+            </div>
+          </div>
+          <img src="../../src/assets/image/abour-img1.png" alt="">
     </div>
-    <h2>核心價值</h2>
     <div class="card">
-      <ul>
-        <li>向民眾宣導購買原型食物</li>
-        <li>介紹有機耕種的在地小農</li>
-        <li>協助在地小農推廣有機作物</li>
-        <li>讓民眾吃得健康</li>
-        <li>土地得以永續</li>
-      </ul>
-      <img src="" alt="">
+        <img src="../../src/assets/image/about-img2.png" alt="">
+      <div class="card-txt">
+        <h3>核心價值</h3>
+        <ul>
+          <li>向民眾宣導購買原型食物</li>
+          <li>介紹有機耕種的在地小農</li>
+          <li>協助在地小農推廣有機作物</li>
+          <li>讓民眾吃得健康</li>
+          <li>土地得以永續</li>
+        </ul>
+      </div>
     </div>
-
   </div>
 
-  <h2>果籽協助小農推廣有機耕種及農產品 <br>
-期望達成聯合國永續發展目標（SDGs）</h2>
+  <h3>果籽協助小農推廣有機耕種及農產品 <br>
+期望達成聯合國永續發展目標（SDGs）</h3>
 <!-- 三個按鈕 -->
 <button class="btn"
     :class="{ 'btn-active': currentIndex === 0 }"
     @click="currentIndex = 0"
-  >12 負責任消費與生產
-  </button>
-  <button class="btn"
+    >11 永續城鄉
+    </button>
+    <button class="btn"
     :class="{ 'btn-active': currentIndex === 1 }"
     @click="currentIndex = 1"
-  >11 永續城鄉
+    >12 負責任消費與生產
   </button>
   <button class="btn"
     :class="{ 'btn-active': currentIndex === 2 }"
@@ -43,19 +50,19 @@
  <div class="sdgs-info">
   <div v-if="currentIndex === 0">
     <div class="SDGs">
-      <img src="../../src/assets/image/SDGs12.png" alt="SDGs12">
-      <div class="sdgs-text">
-        <h4>永續發展目標12:促進負責任的消費。<br></h4>
-        <p>小農種植的農作物通常採用較少化學肥料和農藥，此種耕種方式更環保且對土地影響較小，有助於促進可持續的生產模式。<br>小農所提供的環保的食材，也減少了食物運輸過程中的碳足跡，符合負責任消費的理念。</p>
+      <img src="../../src/assets/image/SDGs11.png" alt="SDGs11">
+          <div class="sdgs-text">
+            <h4>永續發展目標11:永續城鄉。<br></h4>
+            <p>小農經營的傳統農業不僅維護了在地文化，更是生物多樣性的守護者。<br>他們的農地為野生動植物提供棲息地且有助於維護生態系統的平衡。</p>
       </div>
       </div>
     </div>
       <div v-if="currentIndex === 1">
         <div class="SDGs">
-          <img src="../../src/assets/image/SDGs11.png" alt="SDGs11">
-          <div class="sdgs-text">
-            <h4>永續發展目標11:永續城鄉。<br></h4>
-            <p>小農經營的傳統農業不僅維護了在地文化，更是生物多樣性的守護者。<br>他們的農地為野生動植物提供棲息地且有助於維護生態系統的平衡。</p>
+          <img src="../../src/assets/image/SDGs12.png" alt="SDGs12">
+      <div class="sdgs-text">
+        <h4>永續發展目標12:促進負責任的消費。<br></h4>
+        <p>小農種植的農作物通常採用較少化學肥料和農藥，此種耕種方式更環保且對土地影響較小，有助於促進可持續的生產模式。<br>小農所提供的環保的食材，也減少了食物運輸過程中的碳足跡，符合負責任消費的理念。</p>
           </div>
         </div>
       </div>
@@ -85,31 +92,61 @@
 <style lang="scss" scoped>
 @import "@/assets/scss/base/_font.scss";
 @import "@/assets/scss/mixin/mixin.scss";
+// .title-bgi{
+//   position: absolute;
+//       top: 0;
+//       width: 100%;
+//       height: 100%;
+//       max-width: 900px;
+// }
 .aboutus{
   margin: 20px;
   padding: 20px;
 }
 .card{
-  width: 450px;
+  max-width: 550px;
+ display:flex;
+ flex-direction: row;
+ align-items: center;
+ justify-self: center;
+ margin: 0 auto;
+ line-height: 1.5rem;
+ padding: auto;
+}
+.card p,.card ul{
+  color: #FFFFFF;
   background-color: #397D5A;
+  width: 300px;
   border-radius: 20px;
   padding: 20px;
   margin: 20px;
+  list-style: disc;
+  text-align: left;
 }
 section{
   max-width: 1200px;
   text-align: center;
-
-}
-.SDGs{
-  max-width: 1200px;
-  display: flex;
+  
+  }
+  .SDGs{
+    max-width: 1200px;
+    display: flex;
+    // background-image: url('../src/assets/image/sdgs_img2.png');
+    // position: absolute;
 }
 sdgs-text{
   line-height: 2.5rem;
+  // position: relative;
+  
 }
 h2{
   padding: 20px;
+  font-size: 40px;
+  // position: relative;
+  color: #144433;
+}
+h3{
+  line-height: 1.5rem;
 }
 h4{
   font-weight: bold;
